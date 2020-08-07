1869c62576d47478e0c4a28a5aec4283de08ca0f
ChenxiiCheng <chenxic1011@gmail.com>
2020-08-07 14:05:31 -0400

    Updates

---
+++ precache-manifest.db2eab3786ec57b584f56917c053422b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b143bbbe507eff74016088d6c8777c9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "da2b764c77f00cdc8a1d",
+    "url": "./static/css/2.9247a659.chunk.css"
+  },
+  {
+    "revision": "8e4b0d970bc36783fd7a",
+    "url": "./static/css/main.83d665b2.chunk.css"
+  },
+  {
+    "revision": "da2b764c77f00cdc8a1d",
+    "url": "./static/js/2.1af893e8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.1af893e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e4b0d970bc36783fd7a",
+    "url": "./static/js/main.117674be.chunk.js"
+  },
+  {
+    "revision": "41c8ad412007e7420f40",
+    "url": "./static/js/runtime-main.47d83c19.js"
+  },
+  {
+    "revision": "1df3a3cf42035337d8d0ff9ddde449ea",
+    "url": "./static/media/bg1.1df3a3cf.jpg"
+  },
+  {
+    "revision": "0ab7c70da56685277aa68643bdbc2587",
+    "url": "./static/media/github.0ab7c70d.png"
+  },
+  {
+    "revision": "682bd9d69f1d7776a7b10cbfba0f7f9a",
+    "url": "./static/media/login_bg.682bd9d6.png"
+  },
+  {
+    "revision": "63031381d7c63e4cade2bcbbd8695ec2",
+    "url": "./static/media/login_bg2.63031381.png"
+  },
+  {
+    "revision": "996c92bc2c37fba05eb035677f4c9dae",
+    "url": "./static/media/logo.996c92bc.png"
+  },
+  {
+    "revision": "35d2ec3bae4f631827221fab4f06f097",
+    "url": "./static/media/qq.35d2ec3b.png"
+  },
+  {
+    "revision": "360cbdb48102e4033a3da406447ac8f3",
+    "url": "./static/media/register_bg1.360cbdb4.png"
+  },
+  {
+    "revision": "4e16e04866df8b228185ca9347cecc2e",
+    "url": "./static/media/register_bg2.4e16e048.png"
+  },
+  {
+    "revision": "392606aa43286baf8635402acc608fc0",
+    "url": "./static/media/register_bg_1.392606aa.png"
+  },
+  {
+    "revision": "42a3fd8609c6e6afb7d46ee14b673b6d",
+    "url": "./static/media/web-logo.42a3fd86.png"
+  },
+  {
+    "revision": "2b84630be9c1201816ab85ccc9e746ee",
+    "url": "./static/media/wechat.2b84630b.jpeg"
+  },
+  {
+    "revision": "5930ca079e056130701cfca561fe15b9",
+    "url": "./static/media/wechat.5930ca07.png"
+  },
+  {
+    "revision": "53b93097898621567ba1f41014b3e9a8",
+    "url": "./static/media/weibo.53b93097.png"
+  }
+]);
